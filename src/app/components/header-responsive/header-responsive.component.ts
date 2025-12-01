d0443d591c365a9172eb135b3c918a8b072676df
Diego-de-Souza <diegodesouza.souza@gmail.com>
2025-12-01 09:26:05 -0300

    fix(melhorias): melhoria no layout que quebrou do menu responsivo

--- src/app/components/header-responsive/header-responsive.component.ts
+++ src/app/components/header-responsive/header-responsive.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild, HostListener, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { WeatherComponent } from '../weather/weather.component';
 import { ContactWhatsappComponent } from '../contact-whatsapp/contact-whatsapp.component';
@@ -13,32 +13,52 @@ import { ContactWhatsappComponent } from '../contact-whatsapp/contact-whatsapp.c
 export class HeaderResponsiveComponent {
   tipoData: string = 'true';
   @ViewChild('navIcon') navIcon: ElementRef | undefined;
-  
+  isMenuOpen: boolean = false;
+
   constructor(private router:Router, private renderer: Renderer2){  }
-  
+
   openContato(){
     this.router.navigate(['/contato'])
+    this.closeMenu();
   }
-  
+
   openHome(){
     this.router.navigate([''])
+    this.closeMenu();
   }
 
   openConhecimento(){
     this.router.navigate(['/conhecimento'])
+    this.closeMenu();
   }
 
-  openMenu() {
-    const alturaMenu = document.querySelector('.headerResponsive');
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
     if (this.navIcon) {
-      const navElement = this.navIcon.nativeElement;
-      if (navElement.classList.contains('open')) {
-        this.renderer.removeClass(navElement, 'open');
-        this.renderer.setStyle(alturaMenu, 'height', '4em');
+      if (this.isMenuOpen) {
+        this.renderer.addClass(this.navIcon.nativeElement, 'open');
       } else {
-        this.renderer.addClass(navElement, 'open');
-        this.renderer.setStyle(alturaMenu, 'height', 'auto');
+        this.renderer.removeClass(this.navIcon.nativeElement, 'open');
       }
     }
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+    if (this.navIcon) {
+      this.renderer.removeClass(this.navIcon.nativeElement, 'open');
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const header = document.querySelector('.headerResponsive');
+    if (header && !header.contains(event.target as Node) && this.isMenuOpen) {
+      this.closeMenu();
+    }
+  }
+
+  ngOnInit() {
+    this.closeMenu();
   }
 }
